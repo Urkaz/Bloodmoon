d0ed7fec68346097874ea0a3e6201f21a8fc0ce5
Urkaz <mrurkaz@gmail.com>
2020-02-23 19:24:45 +0100

    fix moon colorization crashing the (integrated) server with mods calling getMoonPhase() on the server side

--- src/main/java/lumien/bloodmoon/proxy/ClientProxy.java
+++ src/main/java/lumien/bloodmoon/proxy/ClientProxy.java
@@ -1,24 +1,31 @@
 package lumien.bloodmoon.proxy;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+
 import lumien.bloodmoon.client.ClientBloodmoonHandler;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-public class ClientProxy extends CommonProxy
-{
+public class ClientProxy extends CommonProxy {
 	@Override
-	public void preInit(FMLPreInitializationEvent event)
-	{
+	public void preInit(FMLPreInitializationEvent event) {
 		super.preInit(event);
 
-		FMLCommonHandler.instance().bus().register(ClientBloodmoonHandler.INSTANCE);
 		MinecraftForge.EVENT_BUS.register(ClientBloodmoonHandler.INSTANCE);
 	}
 
 	@Override
-	public boolean isBloodmoon()
-	{
+	public boolean isBloodmoon() {
 		return ClientBloodmoonHandler.INSTANCE.isBloodmoonActive();
+	}
+
+	@Override
+	public boolean canColorMoon() {
+		try {
+			return Display.isCurrent();
+		} catch (LWJGLException e) {
+			return false;
+		}
 	}
 }
